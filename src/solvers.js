84b1d6babf59383793dcb6fbe2171ab34c32e097
hibow <utfnalin@gmail.com>
2019-02-06 22:29:31 -0600

    testing findNQueens

--- src/solvers.js
+++ src/solvers.js
@@ -21,6 +21,7 @@ window.findNRooksSolution = function (n) {
   var startRow = Math.floor(Math.random() * Math.floor(n));
   var startCol = Math.floor(Math.random() * Math.floor(n));
   solution[startRow][startCol] = 1;
+
   for (var r = 0; r < n; r++) {
     if (hasRowValueAt(solution, r)) {
       continue;
@@ -49,7 +50,62 @@ window.countNRooksSolutions = function (n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function (n) {
-  var solution = undefined; //fixme
+  var solution = makeEmptyMatrix(n);
+  if (n === 0) {
+    return solution;
+  }
+  var numberOnes = 0;
+  var previousRow = 0;
+  var startCol = 0;
+  var startRow = 0;
+  debugger;
+  do {
+    numberOnes = 0;
+    // var startRow = Math.floor(Math.random() * Math.floor(n));
+    // var startCol = Math.floor(Math.random() * Math.floor(n));
+    solution[startRow][startCol] = 1;
+    numberOnes ++;
+    //debugger;
+    var majorArr = [];
+    var minorArr = [];
+    for (var r = 0; r < n; r++) {
+      if (hasRowValueAt(solution, r)) {
+        continue;
+      }
+      for (var c = 0; c < n; c++) {
+        if (hasColValueAt(solution, c)) {
+          continue;
+        } else {
+          var majorInx = c - r;
+          var minorInx = c + r;
+          if ( (majorArr[majorArr] !== undefined && majorArr[majorInx] === true)
+            || (minorArr[minorInx] !== undefined && minorArr[minorInx] === true)) {
+            continue;
+          }
+          if (hasDiagonalValueAt(solution, majorInx, minorInx)) {
+            majorArr[majorInx] = true;
+            minorArr[minorInx] = true;
+            continue;
+          }
+          solution[r][c] = 1;
+          numberOnes ++;
+          break;
+        }
+      }
+    }
+    if (startCol < n) {
+      startCol ++;
+    }
+    if (startCol > n) {
+      startCol = 0;
+      previousRow ++;
+      startRow = previousRow;
+    }
+    console.log('numberOnes : ' + numberOnes + ', startRow :' + startRow + ', startCol: ' + startCol);
+  } while (numberOnes < n);
+
+
+
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -84,6 +140,21 @@ window.hasRowValueAt = function (arr, rowIndex) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[rowIndex][i] === 1) {
       return true;
+    }
+  }
+  return false;
+};
+window.hasDiagonalValueAt = function (arr, majorInx, minorInx) {
+  //go thru solution array
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length; j++) {
+      //check if current index has same majorInx or minorInx
+      if ( (j - i === majorInx) || (j + i === minorInx)) {
+        if (arr[i][j] === 1) {
+          //if the value is one, then return true
+          return true;
+        }
+      }
     }
   }
   return false;
