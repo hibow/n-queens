bc2b15216a99abf8f1f1576f5e17ff6709abe580
hibow <utfnalin@gmail.com>
2019-02-10 22:16:54 -0600

    Complete Bare Minimum Requirements

--- src/solvers.js
+++ src/solvers.js
@@ -15,106 +15,176 @@
 
 
 
-window.findNRooksSolution = function (n) {
-  //togglePiece: function (rowIndex, colIndex)
-  var solution = makeEmptyMatrix(n);
-  var startRow = Math.floor(Math.random() * Math.floor(n));
-  var startCol = Math.floor(Math.random() * Math.floor(n));
-  solution[startRow][startCol] = 1;
-
-  for (var r = 0; r < n; r++) {
-    if (hasRowValueAt(solution, r)) {
-      continue;
+var arrayClone = function( arr ) {
+  var i, copy;
+  if ( Array.isArray( arr ) ) {
+    copy = arr.slice( 0 );
+    for ( i = 0; i < copy.length; i++ ) {
+      copy[ i ] = arrayClone( copy[ i ] );
     }
-    for (var c = 0; c < n; c++) {
-      if (hasColValueAt(solution, c)) {
-        continue;
-      } else {
-        solution[r][c] = 1;
-        break;
+    return copy;
+  } else if ( typeof arr === 'object' ) {
+    throw 'Cannot clone array containing an object!';
+  } else {
+    return arr;
+  }
+};
+
+var SolTree = function(solBoard) {
+  this.counter = 0;
+  this.childCount = 0;
+  this.children = [];
+  this.board = solBoard;
+};
+SolTree.prototype.addChild = function(solBoard, row, col) {
+  var cBoard = arrayClone(solBoard);
+  var child = new SolTree(cBoard);
+  child.board[row][col] = 1;
+  child.counter = this.counter + 1;
+  this.children.push(child);
+  this.childCount ++;
+};
+window.findNRooksSolution = function(n) {
+  var solution;
+  //assume each row has solution -> each tree has same amount of children
+  var root = new SolTree(makeEmptyMatrix(n));
+  var recursion = function (tree, length, rowInx) {
+    if (n === 0) {
+      result = makeEmptyMatrix(n);
+      return result;
+    }
+    if (rowInx === length) {
+      if (tree.counter === length) {
+        solution = tree.board;
+        return solution;
       }
     }
-  }
-
+    for (var i = 0; i < length; i++) {
+      if (hasColValueAt(tree.board, i) || hasRowValueAt(tree.board, rowInx)) {
+        continue;
+      } else {
+        tree.addChild(tree.board, rowInx, i);
+      }
+    }
+    if (rowInx <= length - 1 && tree.children.length) {
+      for (var c = 0; c < tree.children.length; c++) {
+        return recursion(tree.children[c], length, rowInx + 1);
+      }
+    }
+  };
+  solution = recursion(root, n, 0);
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function (n) {
-  var solutionCount = undefined; //fixme
-
+window.countNRooksSolutions = function(n) {
+  var root = new SolTree(makeEmptyMatrix(n));
+  var solutionCount = 0;
+  var recursion = function(tree, length, rowInx) {
+    if (rowInx === length && tree.counter === length) {
+      solutionCount ++;
+      return;
+    }
+    for (var i = 0; i < length; i++) {
+      if (!hasColValueAt(tree.board, i) && !hasRowValueAt(tree.board, rowInx)) {
+        tree.addChild(tree.board, rowInx, i);
+        //run func for each added latest child
+        var cLen = tree.childCount - 1;
+        recursion(tree.children[cLen], length, rowInx + 1);
+      }
+    }
+  };
+  recursion(root, n, 0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function (n) {
+window.findNQueensSolution = function(n) {
+  //assume each row has solution-> each tree has child and assume n queens
   var solution = makeEmptyMatrix(n);
-  if (n === 0) {
-    return solution;
-  }
-  var numberOnes = 0;
-  var previousRow = 0;
-  var startCol = 0;
-  var startRow = 0;
-  debugger;
-  do {
-    numberOnes = 0;
-    // var startRow = Math.floor(Math.random() * Math.floor(n));
-    // var startCol = Math.floor(Math.random() * Math.floor(n));
-    solution[startRow][startCol] = 1;
-    numberOnes ++;
-    //debugger;
-    var majorArr = [];
-    var minorArr = [];
-    for (var r = 0; r < n; r++) {
-      if (hasRowValueAt(solution, r)) {
+  var root = new SolTree(makeEmptyMatrix(n));
+  var recursion = function (tree, length, rowInx) {
+    var result;
+    if (n === 0) {
+      result = makeEmptyMatrix(n);
+      return result;
+    }
+    if (rowInx === length) {
+      if (tree.counter === length) {
+        result = tree.board;
+        return result;
+      } else {
+        return;
+      }
+    }
+    for (var i = 0; i < length; i++) {
+      var majI = i - rowInx;
+      var minI = i + rowInx;
+      if (hasColValueAt(tree.board, i) || hasRowValueAt(tree.board, rowInx) || hasDiagonalValueAt(tree.board, majI, minI)) {
         continue;
+      } else {
+        tree.addChild(tree.board, rowInx, i);
       }
-      for (var c = 0; c < n; c++) {
-        if (hasColValueAt(solution, c)) {
-          continue;
-        } else {
-          var majorInx = c - r;
-          var minorInx = c + r;
-          if ( (majorArr[majorArr] !== undefined && majorArr[majorInx] === true)
-            || (minorArr[minorInx] !== undefined && minorArr[minorInx] === true)) {
-            continue;
-          }
-          if (hasDiagonalValueAt(solution, majorInx, minorInx)) {
-            majorArr[majorInx] = true;
-            minorArr[minorInx] = true;
-            continue;
-          }
-          solution[r][c] = 1;
-          numberOnes ++;
-          break;
+    }
+    if (tree.children.length && rowInx <= length - 1) {
+      for (var c = 0; c < tree.children.length; c++) {
+        result = recursion(tree.children[c], length, rowInx + 1);
+        if (result !== undefined) {
+          return result;
         }
+      } //no kids can't go on
+    } else if (rowInx <= length) {
+      result = recursion(tree, length, rowInx + 1);
+      if (result !== undefined) {
+        return result;
       }
     }
-    if (startCol < n) {
-      startCol ++;
-    }
-    if (startCol > n) {
-      startCol = 0;
-      previousRow ++;
-      startRow = previousRow;
-    }
-    console.log('numberOnes : ' + numberOnes + ', startRow :' + startRow + ', startCol: ' + startCol);
-  } while (numberOnes < n);
-
-
-
+  };
+  var result = recursion(root, n, 0);
+  if (result !== undefined) {
+    solution = result;
+  }
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function (n) {
-  var solutionCount = undefined; //fixme
-
+window.countNQueensSolutions = function(n) {
+  //assume each row has solution-> each tree has child and assume n queens
+  var solution = makeEmptyMatrix(n);
+  var solutionCount = undefined;
+  var root = new SolTree(makeEmptyMatrix(n));
+  var recursion = function (tree, length, rowInx) {
+    var tolCount = 0;
+    if (rowInx === length) {
+      if (tree.counter === length) {
+        return tolCount = tolCount + 1;
+      } else {
+        return 0;
+      }
+    }
+    for (var i = 0; i < length; i++) {
+      var majI = i - rowInx;
+      var minI = i + rowInx;
+      if (hasColValueAt(tree.board, i) || hasRowValueAt(tree.board, rowInx) || hasDiagonalValueAt(tree.board, majI, minI)) {
+        continue;
+      } else {
+        tree.addChild(tree.board, rowInx, i);
+      }
+    }
+    if (tree.children.length && rowInx <= length - 1) {
+      for (var c = 0; c < tree.children.length; c++) {
+        tolCount = tolCount + recursion(tree.children[c], length, rowInx + 1);
+      } //no kids can't go on
+    } else if (rowInx <= length) {
+      tolCount = tolCount + recursion(tree, length, rowInx + 1);
+    }
+    return tolCount;
+  };
+  solutionCount = recursion(root, n, 0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
